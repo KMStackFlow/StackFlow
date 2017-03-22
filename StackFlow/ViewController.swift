6bbe7469278f70dc8b3b1a9bfa1a4e02fa2515db
Huai-Che Lu <hc.acsalu@gmail.com>
2017-03-22 16:53:45 -0400

    Initial Commit

---
+++ StackFlow/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  StackFlow
+//
+//  Created by Huai-Che Lu on 3/22/17.
+//  Copyright © 2017 Keymochi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
