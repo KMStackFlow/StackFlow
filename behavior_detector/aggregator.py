19d768e96e3c563b62954b669ef65f0d3cb2c135
inna <ishteinbuk@gmail.com>
2017-05-04 17:29:59 -0400

    adding aggregator.py

---
+++ behavior_detector/aggregator.py
@@ -0,0 +1,64 @@
+import datetime
+import time
+from urllib.parse import urlparse
+from collections import defaultdict
+
+
+class Aggregator(object):
+    """
+        Aggregator for log file.
+        This reads the log file for today
+    """
+    def __init__(self):
+        # time is at 7am
+        st = datetime.datetime.now().replace(hour=7, minute=0,
+                                             second=0, microsecond=0)
+        file_datetime = int(time.mktime(st.timetuple()))
+        print(file_datetime)
+        log_file_path = "behavior_detector/logs/window_" + str(file_datetime) + ".txt"
+        windows = []
+        with open(log_file_path, 'r') as fr:
+            for line in fr:
+                temp = line.split()
+                try:
+                    temp[0] = int(temp[0])
+                    line = line.strip()
+                    timestamp = line[:11].strip()
+                    window_name = line[11:].strip()
+                    windows.append((timestamp, window_name))
+                except:
+                    print("ignoring", line)
+        self.windows = windows
+
+    def parse_window(self, window):
+        try:
+            return window.split(' :: ')
+        except:
+            return window
+
+    def aggregate_by_program(self):
+        """
+            returns json of program name, seconds spent
+            for urls, program name = domain name
+        """
+        aggregate = defaultdict(int)
+        windows = self.windows
+        for idx in range(1, len(windows)):
+            last_ts, window = windows[idx - 1]
+            curr_ts, _ = windows[idx]
+            window_name = self.parse_window(window)
+            if window_name is None:
+                continue
+            if window_name[0] == 'Google Chrome':
+                if len(window_name) > 1:
+                    domain = urlparse(window_name[1]).netloc
+                    # manually change local host to jupyter notebook
+                    if 'localhost' in domain:
+                        domain = 'jupyter notebook'
+                    aggregate[domain] += int(curr_ts) - int(last_ts)
+            else:
+                aggregate[window_name[0]] += int(curr_ts) - int(last_ts)
+        return aggregate
+
+    def aggregate_by_time(self):
+        return {9: 20, 10: 30, 11: 0, 12: 10}
--- behavior_detector/behavior_detector.py
+++ behavior_detector/behavior_detector.py
@@ -1,5 +1,4 @@
 from behavior_detector.ulogme_osx import EventSniffer
-import behavior_detector.ulogme_osx
 import behavior_detector.util as util
 import os
 from urllib.parse import urlparse
@@ -8,92 +7,84 @@ from collections import defaultdict
 import time
 
 
-
 class BehaviorDetector(object):
+    def __init__(self):
 
-	def __init__(self):
+        os.chdir(os.path.dirname(__file__))
+        util.makedir("logs")
+        curr_path = os.getcwd()
+        options = util.Options()
+        options.pid_file = curr_path + ".python_pid"
+        options.keystroke_raw_file = curr_path + "/logs/keyfreqraw.txt"
+        options.active_window_file = curr_path + "/logs/window_%s.txt"
+        options.active_window_time = 2
 
-		os.chdir(os.path.dirname(__file__))
-		util.makedir("logs")
-		curr_path = os.getcwd()
-		options = util.Options()
-		options.pid_file = curr_path + ".python_pid"
-		options.keystroke_raw_file = curr_path + "/logs/keyfreqraw.txt"
-		options.active_window_file = curr_path + "/logs/window_%s.txt"
-		options.active_window_time = 2
-		
-		self.options = options
-		self.event_sniffer = EventSniffer(options=self.options)
-		self.CHROME = ['Google Chrome']
-		# TODO line 26: Safari handles URLs weird. We don't always get an actual URL...
-		self.SAFARI = ['Safari']
-		self.CONTEXT_SWITCHING_SEC = 20
-		self.CONTEXT_SWITCHING_DISTRACT = 0.6
-		self.OFFENSIVE_TIME = 30 #900 #15 min.
+        self.options = options
+        self.event_sniffer = EventSniffer(options=self.options)
+        self.CHROME = ['Google Chrome']
+        # TODO line 26: With Safari we don't get the URL
+        self.SAFARI = ['Safari']
+        self.CONTEXT_SWITCHING_SEC = 20
+        self.CONTEXT_SWITCHING_DISTRACT = 0.6
+        self.OFFENSIVE_TIME = 30  # 900 #15 min.
 
-	def run(self):
-		print("running EventSniffer...\n")
-		self.event_sniffer.run()
+    def run(self):
+        print("running EventSniffer...\n")
+        self.event_sniffer.run()
 
-	
-	def parse_window(self, window):
-		""" Parse window name. Some don't have window name
-		"""
-		try:
-			return window.split(' :: ')
-		except:
-			return window
+    def parse_window(self, window):
+        """ Parse window name. Some don't have window name
+        """
+        try:
+            return window.split(' :: ')
+        except:
+            return window
 
+    def should_breathe(self):
+        """ Parses last records and analyzes
+            Returns: True if user is context_switching or conducting offensive behavior, else False
+            Assumption: Only distracting domains, not programs.
+        """
+        CS = False
+        OB = False
+        records = self.event_sniffer.last_records
+        print(records)
+        aggregate = defaultdict(int)
+        distract_count = 0
+        if records:
+            last_time = None
+            last_window = None
+            time_diff = None
+            for i, r in enumerate(records):
+                window_name = self.parse_window(r.window_name)
+                curr_time = r.timestamp
+                if not window_name:  # HACK: sometimes r.window_name is None
+                    continue
+                if last_time:
+                    time_diff = curr_time - last_time
+                last_time = curr_time
 
-	def should_breathe(self):
-		""" Parses last records and analyzes
-			Returns: True if user is context_switching or conducting offensive behavior, else False
-			Assumption: Only distracting domains, not programs.
-		""" 
-		CS = False
-		OB = False
-		records = self.event_sniffer.last_records
-		aggregate = defaultdict(int)
-		distract_count = 0
-		if records:
-			last_time = None
-			last_window = None
-			time_diff = None
-			for i, r in enumerate(records):
-				window_name = self.parse_window(r.window_name)
-				curr_time = r.timestamp
-				if not window_name: #HACK: sometimes r.window_name is None
-					continue
-				if last_time:
-					time_diff = curr_time - last_time
-				last_time = curr_time
-				
-				if time_diff and last_window in DISTRACT_DOMAINS:
-					aggregate[last_window] += time_diff
+                if time_diff and last_window in DISTRACT_DOMAINS:
+                    aggregate[last_window] += time_diff
 
-				if window_name[0] in self.CHROME:
-					dname = urlparse(window_name[1]).netloc
-					if dname in DISTRACT_DOMAINS:
-						distract_count += 1
-					last_window = dname
-				else: # If program is not chrome, just save program name
-					last_window = window_name[0]
-				distract_ratio = distract_count/float(len(records))
-				
-				if time_diff and time_diff <= self.CONTEXT_SWITCHING_SEC \
-						and distract_ratio >= self.CONTEXT_SWITCHING_DISTRACT:
-					CS = True
-			curr_time_diff = int(time.time()) - curr_time
-			if last_window in DISTRACT_DOMAINS:
-				aggregate[last_window] += curr_time_diff
-			distracted = [i for i in aggregate if aggregate[i] >= self.OFFENSIVE_TIME]
-			if distracted:
-				OB = True
-		if CS or OB:
-			self.event_sniffer.last_records = []
-		return CS or OB
+                if window_name[0] in self.CHROME:
+                    dname = urlparse(window_name[1]).netloc
+                    if dname in DISTRACT_DOMAINS:
+                        distract_count += 1
+                    last_window = dname
+                else:  # If program is not chrome, just save program name
+                    last_window = window_name[0]
+                distract_ratio = distract_count / float(len(records))
 
-		
-
-
-
+                if time_diff and time_diff <= self.CONTEXT_SWITCHING_SEC \
+                        and distract_ratio >= self.CONTEXT_SWITCHING_DISTRACT:
+                    CS = True
+            curr_time_diff = int(time.time()) - curr_time
+            if last_window in DISTRACT_DOMAINS:
+                aggregate[last_window] += curr_time_diff
+            distracted = [i for i in aggregate if aggregate[i] >= self.OFFENSIVE_TIME]
+            if distracted:
+                OB = True
+        if CS or OB:
+            self.event_sniffer.last_records = []
+        return CS or OB
